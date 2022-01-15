af85b66205a7aef67b272124d1a3b6c6d89c6b99
Jim Earley <jearley@captechconsulting.com>
2022-01-15 07:53:49 -0700

    Docker compose script, build-all scripts, POM cleanup, add maven version increment plugin, CORS configration for API Gateway, general configuration and script cleanup.

---
+++ api-gateway/src/main/java/io/xmljim/algorithms/service/api/WebCorsConfiguration.java
@@ -0,0 +1,30 @@
+package io.xmljim.algorithms.service.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class WebCorsConfiguration implements WebFluxConfigurer {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+increment=$1
+args="-D$1"
+
+echo "Args: $increment $args"
+
 # ALGORITHM-MODEL
 cd ./algorithm-model
 echo "** Build algorithm-model"
@@ -38,33 +43,55 @@ mvn clean install > /dev/null
 # ** DISCOVERY-SERVER **
 cd ../discovery-server
 echo "** Build discovery-server"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** discovery server complete ***"
 
 # ** CONFIG-SERVER **
 cd ../config-server
 echo "** Build config-server"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** config server complete ***"
 
 # ** CPI-SERVICE **
 cd ../cpi-service
 echo "** Build cpi-service"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** cpi service complete ***"
 
 # ** STOCK-SERVICE **
 cd ../stock-service
 echo "** Build stock-service"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** stock service complete ***"
 
 # ** STATISTICS-SERVICE **
 cd ../statistics-service
 echo "** Build statistics service"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** statistics service complete ***"
 
 # ** RETIREMENT-SERVICE **
 cd ../retirement-service
 echo "** Build retirement-service"
-mvn clean install jib:build > /dev/null
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** retirement service complete ***"
 
 # ** API-GATEWAY **
 cd ../api-gateway
 echo "** Build API Gateway"
+mvn clean install $args > /dev/null
+echo "  ** Build docker image"
+mvn compile jib:build
+echo "*** API Gateway complete ***"
